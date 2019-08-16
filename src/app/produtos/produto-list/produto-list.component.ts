1e525c225d4779342434a85df36d8ab59c84a8f3
Alisson Youssf <youssfbr@gmail.com>
2019-08-16 17:01:06 -0300

    Populando a grid com dados - Mockando dados

--- src/app/produtos/produto-list/produto-list.component.ts
+++ src/app/produtos/produto-list/produto-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Produto } from '../shared/produto';
 
 @Component({
   selector: 'app-produto-list',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./produto-list.component.css']
 })
 export class ProdutoListComponent implements OnInit {
+  title: string;
+  produtos: Produto[];
 
   constructor() { }
 
   ngOnInit() {
+    this.title =  'Produtos';
+    this.produtos = [
+      { id: 1, nome: 'Hambúrger', descricao: 'Pão, hambúrguer, salada', preco: 10.00 },
+      { id: 2, nome: 'X-Búrguer', descricao: 'Pão, hambúrguer, queijo, salada', preco: 12.00 },
+      { id: 3, nome: 'X-Bacon', descricao: 'Pão, hambúrguer, queijo, bacon, salada', preco: 14.00 },
+      { id: 4, nome: 'X-Tudo', descricao: 'Pão, hambúrguer, queijo, bacon, salada e batata', preco: 16.00 },
+    ];
   }
 
 }
