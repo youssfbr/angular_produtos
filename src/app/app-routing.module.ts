264f1fcdfee0b16e68d48d8033f6bf2fa91bf065
Alisson Youssf <youssfbr@gmail.com>
2019-08-16 17:23:14 -0300

    Configurando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
+import { ProdutoFormComponent } from './produtos/produto-form/produto-form.component';
+import { ProdutoListComponent } from './produtos/produto-list/produto-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'produtos', component: ProdutoListComponent },
+  { path: 'produtos/novo', component: ProdutoFormComponent },
+  { path: 'produtos/editar/:id', component: ProdutoFormComponent },
+  { path: '', redirectTo: '/produtos', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
