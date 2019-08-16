090d488eb9c42b8477faed7937a514945938e035
Alisson Youssf <youssfbr@gmail.com>
2019-08-16 13:01:31 -0300

    Importando FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProdutoFormComponent } from './produtos/produto-form/produto-form.component';
 import { ProdutoListComponent } from './produtos/produto-list/produto-list.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,7 @@ import { ProdutoListComponent } from './produtos/produto-list/produto-list.compo
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
