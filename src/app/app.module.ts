4be32ca9cba5fd3f62e499d0ee83f0f8a989a0dd
Alisson Youssf <youssfbr@gmail.com>
2019-08-16 10:58:02 -0300

    Criado componente grid de produtos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProdutoFormComponent } from './produtos/produto-form/produto-form.component';
+import { ProdutoListComponent } from './produtos/produto-list/produto-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProdutoFormComponent
+    ProdutoFormComponent,
+    ProdutoListComponent
   ],
   imports: [
     BrowserModule,
