b50ade6c6b55eb439e7781742154fe492612caf2
Alisson Youssf <youssfbr@gmail.com>
2019-08-16 21:08:25 -0300

    Corrigido erros e criada classe in-memory-data

---
+++ src/app/shared/in-memory-data.spec.ts
@@ -0,0 +1,7 @@
+import { InMemoryData } from './in-memory-data';
+
+describe('InMemoryData', () => {
+  it('should create an instance', () => {
+    expect(new InMemoryData()).toBeTruthy();
+  });
+});
